668e447c5fa2fa5787138243fb694bbdddba1a34
heyarne <arne@schlueter.is>
2022-09-28 09:21:38 +0200

    Make env buildable using poetry2nix

--- flake.nix
+++ flake.nix
@@ -3,7 +3,13 @@
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs";
-  inputs.poetry2nix.url = "github:nix-community/poetry2nix";
+  inputs.poetry2nix = {
+    url = "github:nix-community/poetry2nix";
+    inputs = {
+      nixpkgs.follows = "nixpkgs";
+      flake-utils.follows = "flake-utils";
+    };
+  };
 
   outputs = { self, nixpkgs, flake-utils, poetry2nix }:
     {
@@ -14,6 +20,19 @@
           # The application
           datasetteListenbrainzImporter = prev.poetry2nix.mkPoetryApplication {
             projectDir = ./.;
+            overrides = prev.poetry2nix.defaultPoetryOverrides.extend ( self: super: {
+              diff-cover = super.diff-cover.overridePythonAttrs (
+                old: {
+                  buildInputs = (old.buildInputs or [ ]) ++ [ self.poetry ];
+                }
+              );
+
+              datasette = super.datasette.overridePythonAttrs (
+                old: {
+                  buildInputs = (old.buildInputs or [ ]) ++ [ self.pytest-runner ];
+                }
+              );
+            });
           };
         })
       ];
@@ -29,14 +48,15 @@
           datasetteListenbrainzImporter = pkgs.datasetteListenbrainzImporter;
         };
 
-        defaultApp = apps.datasetteListenbrainzImporter;
+        defaultApp = pkgs.datasetteListenbrainzImporter;
 
         devShell = pkgs.mkShell rec {
           buildInputs = with pkgs; [
             (python3.withPackages(ps: with ps; [
+              black
               pip
               poetry
-              black
+              pyright
             ]))
           ];
        };
